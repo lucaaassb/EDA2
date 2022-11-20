bd89b7234acd2bce1dccbc12055d63157d603a6f
Lucas Soares Barros <lucassb395@gmail.com>
2022-11-20 17:52:51 -0300

    Ajuste quest A - Lista 1

--- Lista 1/A.c
+++ Lista 1/A.c
@@ -3,41 +3,16 @@
 
 //========================================================//
 
-typedef struct celula
-{
+typedef struct celula{
     int dado;
     struct celula *prox;
 } celula;
 
 //========================================================//
 
-int main()
-{
-    int n, i, x;
-    celula *lista, *nova, *p;
-    printf("Digite o tamanho da lista: ");
-    scanf("%d", &n);
-    lista = NULL;
-    for (i = 0; i < n; i++)
-    {
-        nova = malloc(sizeof(celula));
-        printf("Digite o valor do elemento %d: ", i + 1);
-        scanf("%d", &x);
-        nova->dado = x;
-        nova->prox = lista;
-        lista = nova;
-    }
-    printf("Lista: ");
-    for (p = lista; p != NULL; p = p->prox)
-        printf("%d ", p->dado);
-    printf("\n");
-    return 0;
-}
-
 //========================================================//
 
-void imprime(celula *le)
-{
+void imprime(celula *le){
     celula *p;
     for (p = le; p != NULL; p = p->prox)
         printf("%d\n", p->dado);
@@ -45,11 +20,13 @@ void imprime(celula *le)
 
 //========================================================//
 
-void imprime_rec(celula *le)
-{
-    if (le != NULL)
-    {
+void imprime_rec(celula *le){
+    if (le != NULL){
         printf("%d\n", le->dado);
-        imprime(le->prox);
+        imprime_rec(le->prox);
     }
+    // if (le != NULL){
+    //     printf("%d\n", le->dado);
+    //     imprime(le->prox);
+    // }
 }
